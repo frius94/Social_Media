27e01b12278a7f2bdd01d7617bdd5e376c77c1a1
frius94 <daniel230994@gmail.com>
2019-06-10 01:27:23 +0200

    [TASK] added list friends & delete friends functionality

--- social_media/app/Http/Controllers/HomeController.php
+++ social_media/app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Person_has_person;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $friends = [];
+        $friendsID = Person_has_person::select('person2')->where('person1', Auth::user()->id)->get();
+        foreach ($friendsID as $friendID)
+            $friends[] = User::find($friendID->person2);
+
+        return view('home')->with('friends', $friends);
     }
 }
--- social_media/resources/views/home.blade.php
+++ social_media/resources/views/home.blade.php
@@ -37,12 +37,19 @@
             <div class="card">
                 <div class="card-body">
                     <h4>Friends</h4>
-                    <ul>
-                        <li>
-                            <a href="#">peterpan</a>
-                            <a class="text-danger" href="#">[unfriend]</a>
-                        </li>
-                    </ul>
+                    @if(count($friends) > 0)
+                        @foreach($friends as $friend)
+                            <ul>
+                                <li>
+                                    <a href="/profile/{{$friend->id}}">{{$friend->firstname . ' ' . $friend->lastname}}</a>
+                                    <a class="text-danger"
+                                       href="/removeFriend/{{Auth::user()->id . '/' . $friend->id}}">[unfriend]</a>
+                                </li>
+                            </ul>
+                        @endforeach
+                    @else
+                        <p>No friends available</p>
+                    @endif
                 </div>
             </div>
             <!-- ./friends -->
