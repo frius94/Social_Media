255ce762860f75a2e95e19d87be49106e858bfdb
frius94 <daniel230994@gmail.com>
2019-06-10 16:45:13 +0200

    [TASK] added post listing to home and profile page.

--- social_media/app/Http/Controllers/HomeController.php
+++ social_media/app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Contribution;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,19 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //Get all contributions from friends of logged in user.
+        $contributions = Contribution::whereIn('people_id', function ($query) {
+            $query->select('person2')->from('person_has_people')->where('person1', Auth::user()->id);
+        })->get();
+
+        //Get the name of the author of the contribution and put it into array
+        $contributionUsers = [];
+        foreach ($contributions as $contribution) {
+            $contributionUsers[] = User::select('firstname', 'lastname')->where('id', $contribution->people_id)->get();
+        }
+
+        return view('home')
+            ->with('contributions', $contributions)
+            ->with('contributionUsers', $contributionUsers);
     }
 }
---
+++ social_media/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contribution;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //Get all contributions from logged in user.
+        $contributions = Contribution::where('people_id', Auth::user()->id)->get();
+
+        $user = User::find($id);
+
+        //Get the name of the author of the contribution and put it into array
+        $contributionUsers = [];
+        foreach ($contributions as $contribution) {
+            $contributionUsers[] = User::select('firstname', 'lastname')->where('id', $contribution->people_id)->get();
+        }
+
+        return view('pages.profile')
+            ->with('user', $user)
+            ->with('contributions', $contributions)
+            ->with('contributionUsers', $contributionUsers);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
