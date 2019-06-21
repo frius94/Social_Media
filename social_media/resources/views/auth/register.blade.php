831b49fd1190fc017820cc1543a6b6ba4c932ce3
Chris <christopher.oconnor@santismail.ch>
2019-06-21 08:53:09 +0200

    [UPDATE] Removed Register Birth-Date wrong Date format

--- social_media/resources/views/auth/register.blade.php
+++ social_media/resources/views/auth/register.blade.php
@@ -79,7 +79,7 @@
                         <br>
 
                         <div class="form-group row">
-                            <label for="birthDate" class="col-md-4 col-form-label text-md-right">{{ __('Birth-Date:(Y-m-d)') }}</label>
+                            <label for="birthDate" class="col-md-4 col-form-label text-md-right">{{ __('Birth-Date:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="birthDate" type="date" class="form-control @error('birthDate') is-invalid @enderror" name="birthDate" value="{{ old('birthDate') }}" required autocomplete="birthDate" autofocus>
