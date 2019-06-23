8b8f56bbd7cb4c350e6a3dfac3fb08c4e8dd5414
frius94 <daniel230994@gmail.com>
2019-06-23 22:42:31 +0200

    [FIX] fixed showing add to friends only to people who are not friends

--- social_media/resources/views/inc/addfriend.blade.php
+++ social_media/resources/views/inc/addfriend.blade.php
@@ -1,7 +1,5 @@
-@if(count($friendsID) > 0)
-    @if(!in_array($user->id, $friendsID))
-        <a href="{{action('Person_has_personController@store', ['id' => $user->id])}}">
-            <i class="fas fa-user-plus"></i>
-            Add to friends</a>
-    @endif
+@if(!in_array($user->id, $friendsID) && ($user->id != auth()->user()->getAuthIdentifier()))
+    <a href="{{action('Person_has_personController@store', ['id' => $user->id])}}">
+        <i class="fas fa-user-plus"></i>
+        Add to friends</a>
 @endif
